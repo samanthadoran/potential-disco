bbf0d9031f7d177b71304068f560a09bc248c2f5
Samantha Doran <samanthadoran3@gmail.com>
2016-06-04 18:43:35 -0700

    Finish moving memory closures to mmu from console.lisp

--- console.lisp
+++ console.lisp
@@ -43,56 +43,6 @@
     (setf (aref buttons 7) (if (sdl2:keyboard-state-p :scancode-d) 1 0))
     buttons))
 
-(defun map-memory (n)
-  (setf (NES-ppu:ppu-trigger-nmi-callback (nes-ppu n)) (6502-cpu:trigger-nmi-callback (nes-cpu n)))
-  (setf (NES-ppu:ppu-oam-dma-callback (nes-ppu n)) (lambda (addr) (6502-cpu:read-cpu (nes-cpu n) addr)))
-  (setf (NES-ppu:ppu-oam-stall-adder (nes-ppu n)) (6502-cpu:add-to-stall (nes-cpu n)))
-  (setf
-   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-get (nes-ppu n))) 0)
-   (ppu-to-mapper-read n))
-  (setf
-   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-set (nes-ppu n))) 0)
-   (ppu-to-mapper-write n))
-  (setf
-   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-get (nes-ppu n))) 1)
-   (ppu-to-name-table-read n))
-  (setf
-   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-set (nes-ppu n))) 1)
-   (ppu-to-name-table-write n))
-  (setf
-   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-get (nes-ppu n))) 2)
-   (ppu-to-palette-read n))
-  (setf
-   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-set (nes-ppu n))) 2)
-   (ppu-to-palette-write n))
-  (setf
-   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 0)
-   (cpu-to-cpu-read n))
-  (setf
-   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-set (nes-cpu n))) 0)
-   (cpu-to-cpu-write n))
-  (setf
-   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 1)
-   (cpu-to-ppu-read n))
-  (setf
-   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-set (nes-cpu n))) 1)
-   (cpu-to-ppu-write n))
-  (setf
-   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 2)
-   (cpu-to-io-read n))
-  (setf
-   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-set (nes-cpu n))) 2)
-   (cpu-to-io-write n))
-  (setf
-   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 5)
-   (cpu-to-cart-read n))
-  (setf
-   (NES-controller:controller-buttons-callback
-    (aref
-     (the (simple-array NES-controller:controller 1)(nes-controllers n))
-     0))
-   #'get-buttons))
-
 (defun console-on (n)
   (declare (nes n))
   (NES-ppu:reset-ppu (nes-ppu n))
--- mmu.lisp
+++ mmu.lisp
@@ -112,3 +112,54 @@
              (aref (nes-controllers n) (mod addr 2))
              val)
             0)))
+
+
+(defun map-memory (n)
+  (setf (NES-ppu:ppu-trigger-nmi-callback (nes-ppu n)) (6502-cpu:trigger-nmi-callback (nes-cpu n)))
+  (setf (NES-ppu:ppu-oam-dma-callback (nes-ppu n)) (lambda (addr) (6502-cpu:read-cpu (nes-cpu n) addr)))
+  (setf (NES-ppu:ppu-oam-stall-adder (nes-ppu n)) (6502-cpu:add-to-stall (nes-cpu n)))
+  (setf
+   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-get (nes-ppu n))) 0)
+   (ppu-to-mapper-read n))
+  (setf
+   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-set (nes-ppu n))) 0)
+   (ppu-to-mapper-write n))
+  (setf
+   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-get (nes-ppu n))) 1)
+   (ppu-to-name-table-read n))
+  (setf
+   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-set (nes-ppu n))) 1)
+   (ppu-to-name-table-write n))
+  (setf
+   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-get (nes-ppu n))) 2)
+   (ppu-to-palette-read n))
+  (setf
+   (aref (the (simple-array function 1) (NES-ppu:ppu-memory-set (nes-ppu n))) 2)
+   (ppu-to-palette-write n))
+  (setf
+   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 0)
+   (cpu-to-cpu-read n))
+  (setf
+   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-set (nes-cpu n))) 0)
+   (cpu-to-cpu-write n))
+  (setf
+   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 1)
+   (cpu-to-ppu-read n))
+  (setf
+   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-set (nes-cpu n))) 1)
+   (cpu-to-ppu-write n))
+  (setf
+   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 2)
+   (cpu-to-io-read n))
+  (setf
+   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-set (nes-cpu n))) 2)
+   (cpu-to-io-write n))
+  (setf
+   (aref (the (simple-array function 1) (6502-cpu:cpu-memory-get (nes-cpu n))) 5)
+   (cpu-to-cart-read n))
+  (setf
+   (NES-controller:controller-buttons-callback
+    (aref
+     (the (simple-array NES-controller:controller 1)(nes-controllers n))
+     0))
+   #'get-buttons))
